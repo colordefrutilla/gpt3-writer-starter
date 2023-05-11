b0f2b358e6a3ac764b875e774287c9cd0d76251f
colordefrutilla <112040604+colordefrutilla@users.noreply.github.com>
2023-05-10 23:06:50 -0300

    mejora prompt 1

--- pages/api/english/generate.js
+++ pages/api/english/generate.js
@@ -7,7 +7,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const generateAction = async (req, res) => {
-  const basePromptPrefix = `Create a simple and specific template to start a sustainable strategy in the organization ${req.body.companyName}. The template should be based on the United Nations Sustainable Development Goals and address key aspects in each section: Context Analysis, Goals and Budget, Opportunity Exploration, Monitoring and KPIs, Conclusion. Include 1 key question and a very brief answer as an example, as well as one additional key question without an answer in each section, and tailor the template to the industry ${req.body.industry}. Provide 2 examples of best sustainable practices in the industry ${req.body.industry}.  `;
+  const basePromptPrefix = `Create a simple and specific template to start a sustainable strategy in the organization ${req.body.companyName}. The template should be based on the United Nations Sustainable Development Goals and address key aspects in each section: Context Analysis, Goals and Budget, Opportunity Exploration, Monitoring and KPIs, Conclusion. Include 1 key question and a very brief answer as an example, as well as one additional key question without an answer in each section, and tailor the template to the industry ${req.body.industry}. Provide 2 examples of best sustainable practices in the industry ${req.body.industry}.`;
 
   // Run first prompt
   console.log(`API:${req.body.industry}${req.body.companyName} `);
@@ -16,7 +16,7 @@ const generateAction = async (req, res) => {
     model: "text-davinci-003",
     prompt: `${basePromptPrefix}${req.body.userInput}\n`,
     temperature: 0.75,
-    max_tokens: 2033,
+    max_tokens: 1700,
   });
 
   const basePromptOutput = baseCompletion.data.choices.pop();
